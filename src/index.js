bddcb8abe4ef99098d1aa29ac6a9e03cd3b1b06f
AlexPurhalo <alexpurhalo@gmail.com>
2016-10-31 19:04:17 +0200

    avoiding-session-exiting-after-page-reloading

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
+// Constant import
+import { CREATE_SESSION_SUCCESS } from './constants/sessions';
+
 // Components import via routing
 import routes from './routes';
 
@@ -17,10 +20,14 @@ import '../styles/index.scss'
 import reducers from './reducers';
 
 // Store definition with Middleware applying and Rendering of React Document Object Model (DOM)
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore),
+	reduced_store = createStoreWithMiddleware(reducers), token = localStorage.getItem('token');
+
+// if token exist changes authenticated flag
+token && reduced_store.dispatch({ type: CREATE_SESSION_SUCCESS });
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Provider store={reduced_store}>
 		<Router history={browserHistory} routes={routes} />
 	</Provider>, document.querySelector('#react-application')
 );
